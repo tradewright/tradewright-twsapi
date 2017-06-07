126a1cca77f02327cdf0779815fccfeaf50bb5a5
Richard L King <rlking@aultan.com>
2017-06-07 19:10:29 +0100

    First commit of port of IB's VB sample app

---
+++ SampleApplications/Ib_VB_API_Sample/Logger.vb
@@ -0,0 +1,41 @@
+﻿Imports TradeWright.IBAPI
+Imports TradeWright.Utilities.Logging
+
+Friend Class Logger
+    Implements ILogger
+
+    Private mBaseLogger As FormattingLogger
+
+    Public Sub New(baseLogger As FormattingLogger)
+        mBaseLogger = baseLogger
+    End Sub
+
+    Private Function ILogger_IsLoggable(logLevel As ILogger.LogLevel) As Boolean Implements ILogger.IsLoggable
+        Return mBaseLogger.IsLoggable(translateLogLevel(logLevel))
+    End Function
+
+    Public Sub Log(message As String, Optional moduleName As String = Nothing, Optional procName As String = Nothing, Optional pLogLevel As ILogger.LogLevel = ILogger.LogLevel.Normal) Implements ILogger.Log
+        mBaseLogger.Log(message, moduleName, procName, translateLogLevel(pLogLevel))
+    End Sub
+
+    Private Function translateLogLevel(level As ILogger.LogLevel) As LogLevel
+        Select Case level
+            Case ILogger.LogLevel.None
+                Return LogLevel.None
+            Case ILogger.LogLevel.Severe
+                Return LogLevel.Severe
+            Case ILogger.LogLevel.Warning
+                Return LogLevel.Warning
+            Case ILogger.LogLevel.Info
+                Return LogLevel.Info
+            Case ILogger.LogLevel.Normal
+                Return LogLevel.Normal
+            Case ILogger.LogLevel.Detail
+                Return LogLevel.Detail
+            Case ILogger.LogLevel.HighDetail
+                Return LogLevel.HighDetail
+        End Select
+        Return ILogger.LogLevel.Normal
+    End Function
+
+End Class
---
+++ SampleApplications/Ib_VB_API_Sample/dlgPegBench.vb
@@ -0,0 +1,36 @@
+﻿Imports TradeWright.IBApi
+
+Public Class dlgPegBench
+
+    Dim order As Order
+
+    Sub New(order As Order)
+        ' This is required by the designer.
+        InitializeComponent()
+
+        Me.order = order
+        tbPeggedChangeAmount.Text = NullableDoubleToString(order.PeggedChangeAmount)
+        tbReferenceChangeAmount.Text = NullableDoubleToString(order.ReferenceChangeAmount)
+        tbStartingPrice.Text = NullableDoubleToString(order.StartingPrice)
+        tbStartingReferencePrice.Text = NullableDoubleToString(order.StockRefPrice)
+        cbPeggedChangeType.SelectedIndex = If(order.IsPeggedChangeAmountDecrease, 1, 0)
+        tbReferenceContractId.Text = order.ReferenceContractId
+        tbReferenceContractExchange.Text = order.ReferenceExchange
+    End Sub
+
+    Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+        order.StartingPrice = NullableDoubleFromString(tbStartingPrice.Text)
+        order.StockRefPrice = NullableDoubleFromString(tbStartingReferencePrice.Text)
+        order.PeggedChangeAmount = NullableDoubleFromString(tbPeggedChangeAmount.Text)
+        order.IsPeggedChangeAmountDecrease = cbPeggedChangeType.SelectedIndex
+        order.ReferenceChangeAmount = NullableDoubleFromString(tbReferenceChangeAmount.Text)
+        order.ReferenceContractId = CInt(tbReferenceContractId.Text)
+        order.ReferenceExchange = tbReferenceContractExchange.Text
+
+        Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Close()
+    End Sub
+End Class
